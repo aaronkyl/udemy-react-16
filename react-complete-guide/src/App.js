117a1b2d08536628c5b150ca0d7abc5bc7081615
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-08 12:19:02 -0500

    commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,14 +13,15 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(pers => {
+      return pers.id === id
+    })
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+    this.setState({persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -56,6 +57,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             )
           })}
