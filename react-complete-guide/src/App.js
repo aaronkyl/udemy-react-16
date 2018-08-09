5687505cc819d677ef2673a3bb15bd1fb50978d1
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-08 23:58:23 -0500

    radium

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from 'react'
+import Radium from 'radium'
+import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
   state = {
@@ -43,13 +44,21 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
 
     if (this.state.showPersons) {
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
@@ -90,4 +99,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
