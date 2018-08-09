616103f63c890fd2279a1c9c7a2c705c2d144ccb
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-08 23:47:58 -0500

    add classes dynamically

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -38,7 +38,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -48,6 +49,7 @@ class App extends Component {
     let persons = null;
 
     if (this.state.showPersons) {
+      style.backgroundColor = 'red'
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
@@ -65,10 +67,19 @@ class App extends Component {
       )
     }
 
+    let classes = []
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
