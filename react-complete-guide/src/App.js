f54f28ce956b88456a621c2d9ad9141e4e41c090
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-09 00:33:11 -0500

    Revert "remove Radium"
    This reverts commit 4998d72d229b20ae4a48ccb9e2c5f01313b318f9.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Radium, { StyleRoot } from 'radium'
 import './App.css'
 import Person from './Person/Person'
 
@@ -43,13 +44,21 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
 
     if (this.state.showPersons) {
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
@@ -77,17 +86,19 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React app</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
-export default App;
+export default Radium(App);
