d2eefd01958d77da0622a0a95f6899958d675392
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-09 14:07:44 -0500

    css modules with pseudo selectors

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -37,19 +37,11 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
-      style.backgroundColor = 'red'
+      btnClass = classes.Red
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
@@ -81,7 +73,7 @@ class App extends Component {
         <h1>Hi, I'm a React app</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
