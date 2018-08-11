9b7005cad4460b7f15245946197939d992f9387e
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-11 00:28:53 -0500

    references

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -7,6 +7,7 @@ class Person extends Component {
   constructor(props) {
     super(props)
     console.log('[Person.js] Inside Constructor', props)
+    this.inputElement = React.createRef()
   }
 
   componentWillMount() {
@@ -16,7 +17,7 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()')
     if (this.props.position === 0) {
-      this.inputElement.focus()
+      this.inputElement.current.focus()
     }
   }
 
@@ -28,7 +29,7 @@ class Person extends Component {
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input 
-          ref={(inp) => {this.inputElement = inp}}
+          ref={this.inputElement}
           type="text" 
           value={this.props.name} 
           onChange={this.props.changed} 
