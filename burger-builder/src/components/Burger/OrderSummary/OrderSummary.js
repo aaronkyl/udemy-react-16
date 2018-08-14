da69bff7c6222cd2457f169f89f2bc9eb32ac4b3
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-14 18:29:01 -0500

    lifecycle hook optimizations

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = []
-  for (let ingredient in props.ingredients) {
-    if (props.ingredients[ingredient]) {
-      ingredientSummary.push(<li key={ingredient}><span style={{textTransform: 'capitalize'}}>{ingredient}</span>: {props.ingredients[ingredient]}</li>)
-    }
+class OrderSummary extends Component {
+  // This was a functional component that was turned into a class
+  // to use lifecycle hooks for teaching purposes
+  componentWillUpdate() {
+    console.log('[OrderSummary] componentWillUpdate()')
   }
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>One delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><b>Total: ${props.totalPrice.toFixed(2)}</b></p>
-      <p>Complete order?</p>
-      <Button clicked={props.cancelPurchasing} btnType='Danger' >CANCEL</Button>
-      <Button clicked={props.continuePurchasing} btnType='Success' >CONTINUE</Button>
-    </Aux>
-  )
+
+  render() {
+    const ingredientSummary = []
+    for (let ingredient in this.props.ingredients) {
+      if (this.props.ingredients[ingredient]) {
+        ingredientSummary.push(<li key={ingredient}><span style={{textTransform: 'capitalize'}}>{ingredient}</span>: {this.props.ingredients[ingredient]}</li>)
+      }
+    }
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>One delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><b>Total: ${this.props.totalPrice.toFixed(2)}</b></p>
+        <p>Complete order?</p>
+        <Button clicked={this.props.cancelPurchasing} btnType='Danger' >CANCEL</Button>
+        <Button clicked={this.props.continuePurchasing} btnType='Success' >CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
