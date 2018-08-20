dc905559c074ff3d54dec8ca63360add6c2a5926
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-08-19 22:46:32 -0500

    fetching orders from Firebase

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
-
+import axios from '../../axios-orders'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import Order from '../../components/Order/Order'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount() {
+    axios.get('/orders.json')
+    .then(response => {
+      const fetchedOrders = []
+      for (let key in response.data) {
+        fetchedOrders.push({
+          ...response.data[key],
+          id: key
+         })
+      }
+      console.log(fetchedOrders)
+      this.setState({orders: fetchedOrders, loading: false})
+    })
+    .catch(error => {
+      this.setState({loading: false})
+    })
+  }
+
   render() {
+    let orders = this.state.loading ? <Spinner /> : <Order />
     return (
       <div>
-        <Order />
-        <Order />
+        {orders}
       </div>
     )
   }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
